1dbd2d445c09b56f048cc4726c44f6dd168a1173
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2025-01-16 00:47:09 +0200

    Modify Button Close and Button Sort Event Handlers

--- script.js
+++ script.js
@@ -342,7 +342,7 @@ btnClose.addEventListener("click", function (e) {
   e.preventDefault();
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       (acc) => acc.username === currentAccount.username
@@ -361,6 +361,6 @@ btnClose.addEventListener("click", function (e) {
 let sorted = false;
 btnSort.addEventListener("click", function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
